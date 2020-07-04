27e25a975b3d0a0f78a43e9df79662a8027d77bc
SERGE <nsergearistide@gmail.com>
2020-07-04 18:59:43 -0400

    landingPage-LoginPage-components-completed

---
+++ client/components/LandingPage.js
@@ -0,0 +1,51 @@
+import React from 'react'
+//import ParticlesBg from 'particles-bg'
+import {makeStyles} from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import {Link} from 'react-router-dom'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1)
+    }
+  }
+}))
+
+const LandingPage = () => {
+  const classes = useStyles()
+  return (
+    <div>
+      <section>
+        {/* <ParticlesBg type="lines" num={450} bg={true} /> */}
+        <div className="container-center">
+          <div className="logo-place-holder">Logo</div>
+          <div>
+            <h1 className="big">PLAN YOUR EVENT</h1>
+
+            <h2>
+              Everything your team needs to create an event in one platform.
+              <br />
+            </h2>
+          </div>
+          <div className={classes.root}>
+            <Button variant="contained" color="primary">
+              Get Started
+            </Button>
+            <Link to="/LoginPage">
+              <Button variant="contained" color="secondary">
+                Login
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
+      <div>
+        <h1>Discover</h1>
+      </div>
+      <section className="discover-place-holder">Image</section>
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ client/components/LoginPage.js
@@ -0,0 +1,95 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
+import {auth} from '../store'
+
+class LoginPage extends Component {
+  render() {
+    const {name, displayName, handleSubmit, error} = this.props
+    return (
+      <div className="loginpage-form">
+        <div className="logo-place-holder-login-form">logo</div>
+        {/* <Login /> */}
+        <div>
+          <form onSubmit={handleSubmit} name={name}>
+            <div>
+              <label htmlFor="email">
+                <small>Email</small>
+              </label>
+
+              <input name="email" type="text" />
+            </div>
+            <br />
+            <div>
+              <label htmlFor="password">
+                <small>Password</small>
+              </label>
+
+              <input name="password" type="password" />
+            </div>
+            <br />
+            <div>
+              <button type="submit">{displayName}</button>
+            </div>
+            {error && error.response && <div> {error.response.data} </div>}
+          </form>
+          <a href="/auth/google">{displayName} with Google</a>
+          <br />
+          <h1>Need an account</h1>
+          <br />
+          <div>
+            <button type="submit">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapLogin = state => {
+  return {
+    name: 'login',
+    displayName: 'Login',
+    error: state.user.error
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    handleSubmit(evt) {
+      evt.preventDefault()
+      const formName = evt.target.name
+      const email = evt.target.email.value
+      const password = evt.target.password.value
+      dispatch(auth(email, password, formName))
+    }
+  }
+}
+export default connect(mapLogin, mapDispatch)(LoginPage)
+
+LoginPage.propTypes = {
+  name: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.object
+}
+//export default LoginPage
+
+// export default LoginPage
+
+// import React from 'react'
+// import {Login} from './auth-form'
+
+// const LoginPage = () => {
+//   //console.log('HERERERERE',props)
+//   return (
+
+//     <div>
+//       <h1>yo wasssup</h1>
+//       <Login />
+
+//     </div>
+//   )
+// }
+
+// export default LoginPage
