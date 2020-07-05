7c8510a284fe4aca5f434017299bcc4185cb023a
Tatiana Aviles <tavilesa12@gmail.com>
2020-07-05 00:55:38 -0400

    feat: added user home and event form component

---
+++ client/components/EventDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+export class EventDetails extends React.Component {
+  render() {
+    return (
+      <div className="form">
+        <p>HI</p>
+      </div>
+    )
+  }
+}
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-  const {email} = props
+  console.log('PROPS', props)
+  const {firstName, lastName, email, profile_pic} = props.user
+  const events = props.events || []
+  const tasks = props.tasks || []
 
   return (
     <div>
-      <h3>Welcome, {email}</h3>
+      <div className="profile">
+        <img src={profile_pic} />
+        <h4>
+          {firstName} {lastName}
+        </h4>
+        <p>{email}</p>
+        <Link to="/addEvent">
+          <button type="submit">Add Event</button>
+        </Link>
+      </div>
+      <div>
+        <h4>Your Events</h4>
+        <ul>{events.map(event => <li key={event.id}>{event.title}</li>)}</ul>
+        <h4>Your Tasks</h4>
+        <ul>{tasks.map(task => <li key={task.id}>{task.title}</li>)}</ul>
+      </div>
     </div>
   )
 }
@@ -20,7 +39,9 @@ export const UserHome = props => {
  */
 const mapState = state => {
   return {
-    email: state.user.email
+    user: state.user,
+    events: state.user.events,
+    tasks: state.user.tasks
   }
 }
 
