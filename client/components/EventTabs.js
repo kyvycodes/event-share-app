72ba28d76e807b4eeef9151cd49d534e82cdd554
Tatiana Aviles <tavilesa12@gmail.com>
2020-07-16 10:35:03 -0400

    fix: edit/add event date and time fields

--- client/components/EventTabs.js
+++ client/components/EventTabs.js
@@ -27,8 +27,8 @@ class EventTabs extends React.Component {
     const eventId = this.props.match.params.id
     const currEvent = this.props.currEvent || []
     return (
-      <div className="eventNavBar">
-        <div>
+      <div>
+        <div display="flex">
           <Box display="flex" justifyContent="center">
             <Typography
               align="center"
