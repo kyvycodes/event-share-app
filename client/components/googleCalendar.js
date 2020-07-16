7bb0e39776079f2d1074289089ea2cd554fdbfdb
Kay <kyv.hardeman@gmail.com>
2020-07-16 17:18:38 -0400

    errors: google

---
+++ client/components/googleCalendar.js
@@ -0,0 +1,34 @@
+// import ApiCalendar from 'react-google-calendar-api/ApiCalendar';
+
+// import React, {ReactNode, SyntheticEvent} from 'react';
+// import ApiCalendar from 'react-google-calendar-api';
+
+// export default class GoogleCalendar extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.handleItemClick = this.handleItemClick.bind(this);
+//     }
+
+//     public handleItemClick(event: SyntheticEvent<any>, name: string): void {
+//       if (name === 'sign-in') {
+//         ApiCalendar.handleAuthClick();
+//       } else if (name === 'sign-out') {
+//         ApiCalendar.handleSignoutClick();
+//       }
+//     }
+
+//     render(): ReactNode {
+//       return (
+//             <button
+//                 onClick={(e) => this.handleItemClick(e, 'sign-in')}
+//             >
+//               sign-in
+//             </button>
+//             <button
+//                 onClick={(e) => this.handleItemClick(e, 'sign-out')}
+//             >
+//               sign-out
+//             </button>
+//         );
+//     }
+// }
