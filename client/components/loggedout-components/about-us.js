3de8728879376a66957e89b2719ea92ffc101f6b
Kay <kyv.hardeman@gmail.com>
2020-07-19 13:09:49 -0400

    done: about-us page completed

--- client/components/loggedout-components/about-us.js
+++ client/components/loggedout-components/about-us.js
@@ -1,169 +1,86 @@
-// import React from 'react'
-// import {
-//   Container,
-//   Grid,
-//   Typography
-// } from '@material-ui/core'
-
-// export const AboutUs = () => {
-//   return (
-//     <div>
-//       <Container className="bt">
-//         <Typography
-//           variant="h4"
-//           component="h2"
-//           gutterBottom
-//         >
-//           Discover
-//         </Typography>
-
-//         <Grid container spacing={3}>
-//           <Grid item xs={6} md={3}>
-//             <img
-//               src="hero-background.jpg"
-//               alt="Lake"
-//               style={{width: '100%'}}
-//             />
-//              <Typography>
-//              Our app will put the fun back into
-//           planning for get-togethers, reunions, parties, graduations and
-//           celebrations of all types. EventShare is the best collaborative event
-//           planning tool there is. We want your special occasion to be the best it
-//           can be!
-//           </Typography>
-//           </Grid>
-//           <Grid item xs={6} md={3}>
-//             <img
-//               src="6.jpg"
-//               alt="Lake"
-//               style={{width: '100%', marginBottom: '1rem'}}
-//             />
-//           </Grid>
-//           <Grid item xs={6} md={3}>
-//             <img
-//               src="1.jpg"
-//               alt="Lake"
-//               style={{width: '100%', marginBottom: '1rem'}}
-//             />
-//           </Grid>
-//           <Grid item xs={6} md={3}>
-//             <img
-//               src="2.jpg"
-//               alt="Lake"
-//               style={{width: '100%', marginBottom: '1rem'}}
-//             />
-//           </Grid>
-//         </Grid>
-//       </Container>
-//     </div>
-//   )
-// }
-
 import React from 'react'
-import {makeStyles} from '@material-ui/core/styles'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Typography from '@material-ui/core/Typography'
-import Avatar from '@material-ui/core'
-// import kyvz from "./kyvz-avatar.png"
-
-const useStyles = makeStyles({
-  // root: {
-  //   maxWidth: 345,
-  // },
-  media: {
-    height: 140
-  },
-  uniform: {
-    textAlign: 'center'
-  }
-})
 
 export const AboutUs = () => {
-  const classes = useStyles()
-
   return (
-    <Card className={classes.uniform}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          component="img"
-          alt="Kay"
-          height="140"
-          // image={kyvz}
-          // image={kyvz}
-          // src="https://www.dropbox.com/h?preview=kyvz-avatar.png"
-        />
-        {/* <Avatar src={kyvz} alt="Kay"/> */}
+    <div>
+      <header>
+        <div>
+          <div>
+            <div>
+              <div className="about-header">
+                <h1 className="h1-header">Hello there,</h1>
+                <h4 className="h4-header">
+                  A creative bunch who love code and design.
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div>
+        <div align="center">
+          <h2 align="center" className="team">
+            Our Team:
+          </h2>
 
-        {/* className={classes.media} */}
-        {/* <Avatar src={kyvz} alt="Kay"/> */}
-
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            Kay Hardeman
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <img
+            height="300px"
+            width="300px"
+            style={{margin: '20px'}}
+            src="/kyvz-avatar.png"
+            alt="Kay"
+          />
+          <h4 className="h4-about">Kay Hardeman</h4>
+          <p style={{margin: '10px'}}>
             I have dealt with what seems like the impossible and persevered,
             preparing me for my new endeavor of becoming a full stack web
             developer.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          // image="/static/images/cards/contemplative-reptile.jpg"
-          title="Tatiana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            Tatiana Aviles
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          </p>
+
+          <img
+            height="300px"
+            width="300px"
+            style={{margin: '20px'}}
+            src="/tatiana-avatar.png"
+            alt="Tatiana"
+          />
+          <h4 className="h4-about">Tatiana Aviles</h4>
+          <p style={{margin: '10px'}}>
+            {' '}
             I’m ready to embrace change, switch careers and put my skills and
             passion to work.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+          </p>
 
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          // image="/static/images/cards/contemplative-reptile.jpg"
-          title="Serge"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            Serge Aristide Nikiema
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            I really enjoy working with technology precisely web development
+          <img
+            height="300px"
+            width="300px"
+            style={{margin: '20px'}}
+            src="/serge-avatar.png"
+            alt="Serge"
+          />
+          <h4 className="h4-about">Serge Aristide Nikiema</h4>
+          <p style={{margin: '10px'}}>
+            /I really enjoy working with technology precisely web development
             tools and I wish to use my skills and contribute to big creation
             within a company.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+          </p>
+          <p style={{margin: '10px'}} />
 
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          // image="/static/images/cards/contemplative-reptile.jpg"
-          title="Luis"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h6" component="h2">
-            Luis Carbajal
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <img
+            height="300px"
+            width="300px"
+            style={{margin: '20px'}}
+            src="luis-avatar.png"
+            alt="Luis"
+          />
+          <h4 className="h4-about">Luis Carbajal</h4>
+          <p style={{margin: '10px'}}>
+            {' '}
             My ultimate goal is to build great products that help people to
             interact with information and the world in a new way.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
