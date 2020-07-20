119065ebcd4a9941201f272e646f472fd7c40f5b
Kay <kyv.hardeman@gmail.com>
2020-07-20 11:29:16 -0400

    fix: route for log in

--- client/components/loggedout-components/why-event-share.js
+++ client/components/loggedout-components/why-event-share.js
@@ -71,53 +71,9 @@ export const EventShare = () => {
           </p>
           <br />
           <br />
+          <br />
         </div>
       </div>
     </div>
   )
 }
-//   return (
-//     <div className="cover-why">
-//     <div className="div-why">
-//       <h1 className="h1-why">Why Event Share ?</h1>
-//       <br />
-//       <Typography variant="h5" className="typed-why">
-//         <Typed
-//           strings={[
-//             'POLLING',
-//             'INVITE',
-//             'CREATE TASKS',
-//             'RVSP',
-//             'ADD TO CALENDAR',
-//             'GET DIRECTIONS',
-//             'ADD PICTURES',
-//             'CHAT🎉'
-//           ]}
-//           style={{color: '#FF5757'}}
-//           style={{font-family: 'Amatic SC}}
-
-//           typeSpeed={40}
-//           backSpeed={60}
-//           loop
-//         />
-//       </Typography>
-//       <br />
-//       <p className="a-why">
-//         {' '}
-//         EventShare can help everyone create a successful event. Our flexible
-//         features encourage hosts to recruit multi-person planning teams to
-//         manage logistics and details for events large and small.</p>
-//         <p className="b-why">We want to put the fun back into planning! Our goal is for hosts and guests to work together in a organized collaborative manner. We are ready to help you with get-togethers, reunions, parties, graduations and celebrations of all types.</p>
-//         <p className="c-why">EventShare is the best
-//         collaborative event planning tool there is. We want your special
-//         occasion to be the best it can be.
-//         <a href="/signup" className="a-why">
-//           {' '}
-//           Try it Out
-//         </a>{' '}
-//         today!
-//       </p>
-//     </div>
-//   </div>
-//   )
-// }
