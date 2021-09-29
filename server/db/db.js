7d11133c3483797a54a2bc7e311517ae9bd0591d
Kay <kyv.hardeman@gmail.com>
2021-09-29 19:11:04 -0400

    updating db options

--- server/db/db.js
+++ server/db/db.js
@@ -6,9 +6,13 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false,
-    dialectOptions: {
-      ssl: true
+    options: {
+      dialect: 'postgres',
+      native: true,
+      ssl: true,
+      dialectOptions: {
+        ssl: true
+      }
     }
   }
 )
